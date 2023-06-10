c51f12281f6ec13f91f8f3900ed0c9ec541ed0a5
bpereira74 <67373077+bpereira74@users.noreply.github.com>
2023-06-10 15:22:47 +0000

    Modificacion Numaleatorio repeticion

--- Numaleatorio.rb
+++ Numaleatorio.rb
@@ -1,26 +1,17 @@
-#te solicitan un programa que simule el juego de “Adivina el
-#número”. En este programa el computador va a elegir un número aleatorio y el
-#usuario al seleccionar un número debe indicarle si ganó o perdió. La condición para
-#ganar es que el número del computador debe ser igual al del jugador.
-#El rango de número va entre 1 hasta n, tomando en consideración que mientras más
-#amplio sea el rango más duradero será el juego.
-
-def Numero_Aleatorio(num1,r1)
-numero_maq=rand(3).to_i
-r1=numero_maq
-  if r1==num1
-  puts("Tu numero es #{num1} y el mio #{r1}. Son iguales: Has ganado") 
-  else
-          puts("Tu numero es #{num1} y el mio #{r1}. Son distintos: Perdiste")
-  end
+      numero_maq=rand(10).to_i
+      r1=numero_maq
+      puts("Este es el juego de adivina mi numero. ")
+      num1=''
+until r1==num1 do 
+      puts("elije numero Elige un numero del 1 al 10")
+      num1=gets.to_i
+      if r1==num1
+            puts("Adivinaste!! mi numero era el #{r1}: Has ganado") 
+      else
+            puts("Ese no es mi numero, inténtalo nuevamente")
+      end
 end
-    number=rand(3).to_i
-    random_number=number
-    puts("***¡Bienvenido/a al Juego adivina un numero!***")
-    puts "Ingresa un numero del 0 al 2"
-    number_select = gets.chomp
-    puts "Ahora me toca: "
-    resultado =           
-    Numero_Aleatorio(number_select,random_number)
-    puts(resultado)
+
+      
+
       
